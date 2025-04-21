19b655b6acd00b33f3c0153c9dfb1dff6dc54050
Paul Orzel <pb.orzel@proton.me>
2025-04-21 16:36:55 +0200

    switch to shadcn

--- tedbot-ui/svelte.config.js
+++ tedbot-ui/svelte.config.js
@@ -1,4 +1,3 @@
-import {preprocessMeltUI, sequence} from "@melt-ui/pp";
 // Tauri doesn't have a Node.js server to do proper SSR
 // so we will use adapter-static to prerender the app (SSG)
 // See: https://v2.tauri.app/start/frontend/sveltekit/ for more info
@@ -6,9 +5,12 @@ import adapter from "@sveltejs/adapter-static";
 import {vitePreprocess} from "@sveltejs/vite-plugin-svelte";
 /** @type {import('@sveltejs/kit').Config}*/
 const config = {
-  preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
+  preprocess: [vitePreprocess()],
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+	alias: {
+		"@/*": "./src/*"
+	}
   }
 };
 export default config;
