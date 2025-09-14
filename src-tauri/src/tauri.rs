6345a51fa455304d8566f3ba21c90c7944e16248
Teddy <pb.orzel@proton.me>
2025-09-14 17:43:19 +0200

    feat: setup to auto-generate ts bindings

--- src-tauri/src/tauri.rs
+++ src-tauri/src/tauri.rs
@@ -1,14 +1,38 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+use specta_typescript::Typescript;
+use tauri_specta::{collect_commands, Builder};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
+#[specta::specta]
 fn greet(name: &str) -> String {
 	format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+	let builder = Builder::<tauri::Wry>::new()
+		.commands(collect_commands![greet]);
+
+	#[cfg(debug_assertions)] // <- Only export on non-release builds
+    builder
+        .export(Typescript::default(), "../src/lib/bindings.ts")
+        .expect("Failed to export typescript bindings");
+
 	tauri::Builder::default()
-		.plugin(tauri_plugin_opener::init())
-		.invoke_handler(tauri::generate_handler![greet])
-		.run(tauri::generate_context!())
-		.expect("error while running tauri application");
+        // and finally tell Tauri how to invoke them
+        .invoke_handler(builder.invoke_handler())
+        .setup(move |app| {
+            // This is also required if you want to use events
+            builder.mount_events(app);
+
+            Ok(())
+        })
+        // on an actual app, remove the string argument
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
