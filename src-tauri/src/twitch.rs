480658832f721673dcaab10f5dc967b55a1775ee
Teddy <pb.orzel@proton.me>
2025-09-15 20:50:04 +0200

    feat: add a global twitch client

--- src-tauri/src/twitch.rs
+++ src-tauri/src/twitch.rs
@@ -1,5 +1,36 @@
+use std::sync::{Arc, LazyLock};
+
+use tokio::sync::RwLock;
 use twitch_api::HelixClient;
+use twitch_oauth2::UserToken;
 
 pub mod auth;
 
-type TwitchClient = HelixClient<'static, reqwest::Client>;
+pub static TWITCH_CLIENT: LazyLock<Arc<RwLock<TwitchClient>>> =
+	LazyLock::new(|| Arc::new(RwLock::new(TwitchClient::new())));
+
+pub struct TwitchClient {
+	client: HelixClient<'static, reqwest::Client>,
+	token: Option<Box<UserToken>>,
+}
+
+impl TwitchClient {
+	pub fn new() -> Self {
+		Self {
+			client: HelixClient::default(),
+			token: None,
+		}
+	}
+
+	pub fn is_logged_in(&self) -> bool {
+		self.token.is_some()
+	}
+
+	pub fn set_token(&mut self, tkn: Box<UserToken>) {
+		self.token = Some(tkn)
+	}
+
+	pub fn get_username(&self) {
+		// TODO
+	}
+}
