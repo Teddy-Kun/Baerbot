7cb2a25aab7f75c8265572f0f8f1a2645b3a79e3
Teddy <pb.orzel@proton.me>
2025-09-15 20:51:28 +0200

    feat: use global twitch client for auth

--- src-tauri/src/twitch/auth.rs
+++ src-tauri/src/twitch/auth.rs
@@ -4,19 +4,29 @@ use std::{
 	io::Write,
 };
 use tokio::time::sleep;
-use twitch_api::HelixClient;
 use twitch_oauth2::{AccessToken, ClientId, DeviceUserTokenBuilder, Scope, UserToken};
 
 use crate::{
 	config::ARGS,
 	error::{Error, ErrorMsg},
-	twitch::TwitchClient,
+	twitch::{TWITCH_CLIENT, TwitchClient},
 };
+
+impl TwitchClient {
+	pub async fn login() -> Result<UserToken, ErrorMsg> {
+		let token = match load_token().await {
+			Ok(tkn) => tkn,
+			Err(_) => twitch_auth().await?, // why we failed, doesn't really matter, just log in with the browser
+		};
+
+		Ok(token)
+	}
+}
 
 pub async fn twitch_auth() -> Result<UserToken, Error> {
 	let tkn = internal_twitch_auth()
 		.await
-		.map_err(|e| e.set_msg(ErrorMsg::TwitchAuth))?;
+		.map_err(|e| e.try_set_msg(ErrorMsg::TwitchAuth))?;
 	Ok(tkn)
 }
 
@@ -24,17 +34,21 @@ async fn internal_twitch_auth() -> Result<UserToken, Error> {
 	let client_id = ClientId::new("15xr4zw5ue7jxpbvt0jwwrwywqch9a".to_string());
 	let scopes = vec![Scope::ChatEdit, Scope::ChatRead];
 
-	let client: HelixClient<reqwest::Client> = HelixClient::default();
-
 	let mut builder = DeviceUserTokenBuilder::new(client_id, scopes);
-	let code = builder.start(&client).await?;
+
+	let client = TWITCH_CLIENT.clone();
+
+	let client = client.read().await;
+	let code = builder.start(&client.client).await?;
 
 	tracing::debug!("code: {:#?}", code);
 
 	let url = code.verification_uri.clone();
 	open::that(url)?;
 
-	let token = builder.wait_for_code(&client, sleep).await?;
+	let token = builder.wait_for_code(&client.client, sleep).await?;
+
+	drop(client);
 
 	if let Err(e) = save_token(&token).await {
 		tracing::warn!("Failed to save token: {:#?}", e);
@@ -49,7 +63,7 @@ async fn internal_twitch_auth() -> Result<UserToken, Error> {
 async fn save_token(token: &UserToken) -> Result<(), Error> {
 	internal_save_token(token)
 		.await
-		.map_err(|e| e.set_msg(ErrorMsg::TokenSave))?;
+		.map_err(|e| e.try_set_msg(ErrorMsg::TokenSave))?;
 	Ok(())
 }
 
@@ -69,19 +83,21 @@ async fn internal_save_token(token: &UserToken) -> Result<(), Error> {
 	Ok(())
 }
 
-pub async fn load_token(client: &TwitchClient) -> Result<UserToken, Error> {
-	let tkn = internal_load_token(client)
+pub async fn load_token() -> Result<UserToken, Error> {
+	let tkn = internal_load_token()
 		.await
-		.map_err(|e| e.set_msg(ErrorMsg::TokenLoad))?;
+		.map_err(|e| e.try_set_msg(ErrorMsg::TokenLoad))?;
 	Ok(tkn)
 }
 
-async fn internal_load_token(client: &TwitchClient) -> Result<UserToken, Error> {
+async fn internal_load_token() -> Result<UserToken, Error> {
 	let conf = &ARGS;
+	let client = TWITCH_CLIENT.clone();
+	let client = client.read().await;
 	if let Some(file) = conf.token_file.clone() {
 		let token_str = fs::read_to_string(file.as_ref())?;
 
-		return Ok(UserToken::from_token(client, AccessToken::new(token_str)).await?);
+		return Ok(UserToken::from_token(&client.client, AccessToken::new(token_str)).await?);
 	}
 
 	// TODO: figure out why this dies on Linux, KDE issue?
@@ -90,7 +106,7 @@ async fn internal_load_token(client: &TwitchClient) -> Result<UserToken, Error> 
 	let access_token = entry.get_secret().await?;
 	let access_token = AccessToken::new(access_token);
 
-	let token = UserToken::from_token(client, access_token).await?;
+	let token = UserToken::from_token(&client.client, access_token).await?;
 
 	Ok(token)
 }
