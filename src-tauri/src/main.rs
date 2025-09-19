fa82bbd1824b679c1993b4cd6299bee11926de6a
Teddy <pb.orzel@proton.me>
2025-09-19 20:16:13 +0200

    fix: setup tracing subsciber as global

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,17 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tracing::Level;
+
 mod tauri;
 
 fn main() {
-	tracing_subscriber::fmt::init();
+	let sub = tracing_subscriber::fmt()
+		.with_max_level(Level::INFO)
+		.finish();
+	if let Err(err) = tracing::subscriber::set_global_default(sub) {
+		eprintln!("Error setting up logger: {}", err)
+	}
+	// tedbot_lib::tts::tts();
 	tauri::run()
 }
