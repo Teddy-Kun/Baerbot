5c0a733d0abdccea454ed1b61848b72c00625bbc
Teddy <pb.orzel@proton.me>
2025-09-28 11:46:20 +0200

    refactor: don't require Option in from_err fn

--- src-tauri/src/error.rs
+++ src-tauri/src/error.rs
@@ -38,9 +38,12 @@ impl Error {
 		Self { msg, src: None }
 	}
 
-	pub fn from_err(src: Option<anyhow::Error>, msg: ErrorMsg) -> Self {
+	pub fn from_err(src: anyhow::Error, msg: ErrorMsg) -> Self {
 		// TODO: log error
-		Self { src, msg }
+		Self {
+			src: Some(src),
+			msg,
+		}
 	}
 
 	pub fn try_set_msg(mut self, msg: ErrorMsg) -> Self {
@@ -76,13 +79,13 @@ impl From<ErrorMsg> for Error {
 
 impl From<std::io::Error> for Error {
 	fn from(value: std::io::Error) -> Self {
-		Self::from_err(Some(value.into()), ErrorMsg::Unknown)
+		Self::from_err(value.into(), ErrorMsg::Unknown)
 	}
 }
 
 impl From<keyring::Error> for Error {
 	fn from(value: keyring::Error) -> Self {
-		Self::from_err(Some(value.into()), ErrorMsg::Unknown)
+		Self::from_err(value.into(), ErrorMsg::Unknown)
 	}
 }
 
@@ -90,56 +93,62 @@ type ThreadsafeError = dyn StdError + Send + Sync + 'static;
 
 impl From<ValidationError<&ThreadsafeError>> for Error {
 	fn from(value: ValidationError<&ThreadsafeError>) -> Self {
-		Self::from_err(Some(value.into()), ErrorMsg::Unknown)
+		Self::from_err(value.into(), ErrorMsg::Unknown)
 	}
 }
 
 impl From<ValidationError<CompatError<reqwest::Error>>> for Error {
 	fn from(value: ValidationError<CompatError<reqwest::Error>>) -> Self {
-		Self::from_err(Some(value.into()), ErrorMsg::Unknown)
+		Self::from_err(value.into(), ErrorMsg::Unknown)
 	}
 }
 
 impl From<DeviceUserTokenExchangeError<CompatError<reqwest::Error>>> for Error {
 	fn from(value: DeviceUserTokenExchangeError<CompatError<reqwest::Error>>) -> Self {
-		Self::from_err(Some(value.into()), ErrorMsg::Unknown)
+		Self::from_err(value.into(), ErrorMsg::Unknown)
 	}
 }
 
 impl From<toml::ser::Error> for Error {
 	fn from(value: toml::ser::Error) -> Self {
-		Self::from_err(Some(value.into()), ErrorMsg::Unknown)
+		Self::from_err(value.into(), ErrorMsg::Unknown)
 	}
 }
 
 impl From<toml::de::Error> for Error {
 	fn from(value: toml::de::Error) -> Self {
-		Self::from_err(Some(value.into()), ErrorMsg::Unknown)
+		Self::from_err(value.into(), ErrorMsg::Unknown)
 	}
 }
 
 impl From<twitch_irc::Error<SecureTCPTransport, StaticLoginCredentials>> for Error {
 	fn from(value: twitch_irc::Error<SecureTCPTransport, StaticLoginCredentials>) -> Self {
-		Self::from_err(Some(value.into()), ErrorMsg::ChatMsgSend)
+		Self::from_err(value.into(), ErrorMsg::ChatMsgSend)
 	}
 }
 
 impl From<twitch_irc::validate::Error> for Error {
 	fn from(value: twitch_irc::validate::Error) -> Self {
-		Self::from_err(Some(value.into()), ErrorMsg::ChatMsgSend)
+		Self::from_err(value.into(), ErrorMsg::ChatMsgSend)
+	}
+}
+
+impl From<anyhow::Error> for Error {
+	fn from(value: anyhow::Error) -> Self {
+		Self::from_err(value, ErrorMsg::Unknown)
 	}
 }
 
 #[cfg(target_os = "windows")]
 impl From<windows::core::Error> for Error {
 	fn from(value: windows::core::Error) -> Self {
-		Self::from_err(Some(value.into()), ErrorMsg::GetColorScheme)
+		Self::from_err(value.into(), ErrorMsg::GetColorScheme)
 	}
 }
 
 #[cfg(target_os = "windows")]
 impl From<hex::FromHexError> for Error {
 	fn from(value: hex::FromHexError) -> Self {
-		Self::from_err(Some(value.into()), ErrorMsg::GetColorScheme)
+		Self::from_err(value.into(), ErrorMsg::GetColorScheme)
 	}
 }
