6bc85403360f899bec98ca67743e70e8df8f0caf
Teddy-Kun <pb.orzel@proton.me>
2025-12-21 17:57:23 +0100

    feat: add dialog backdrop blur

--- src/lib/components/app_sidebar.svelte
+++ src/lib/components/app_sidebar.svelte
@@ -43,13 +43,13 @@
 			size="icon"
 		>
 			<span
-				class="h-full w-[1.5rem] absolute flex items-center justify-center left-4"
+				class="h-full w-6 absolute flex items-center justify-center left-4"
 			>
 				<SunIcon
-					class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 !transition-all dark:-rotate-90 dark:scale-0"
+					class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all! dark:-rotate-90 dark:scale-0"
 				/>
 				<MoonIcon
-					class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 !transition-all dark:rotate-0 dark:scale-100"
+					class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all! dark:rotate-0 dark:scale-100"
 				/>
 			</span>
 			<span class="w-full">Toggle theme</span>
